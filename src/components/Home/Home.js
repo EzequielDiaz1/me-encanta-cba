cf6bc1ff68508e5fe344da29f756b26e47f0a343
Ezequiel Diaz <63514491+EzequielDiaz1@users.noreply.github.com>
2022-04-29 16:20:03 -0300

    Navbar creado, Home creado, fotos descargadas

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import pattern from '../../pattern.png'
+import styles from './Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>HOLA</div>
+  )
+}
+
+export default Home
